99a8d801db0ab09011f0db6462156526e5d9cb96
sakunasanka <sakunasanka@gmail.com>
2024-09-02 22:29:40 +0530

    Create instructor crud

--- backend/controllers/instructorControllers.js
+++ backend/controllers/instructorControllers.js
@@ -12,7 +12,71 @@ const createInstructor = async (req,res)=>{
     } 
 }
 
+//get all instructors details
+const getAllInstructorDetails = async (req,res) =>{
+    const details = await instructors.find({}).sort({createdAt : -1})
+ 
+    res.json(details);
+}
+
+//get single student details
+const getSingleInstructorDetails = async (req,res) =>{
+    const {id} = req.params
+
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        res.status(404).json({error: 'No such instructor'})
+    }
+
+    const details = await instructors.findById(id)
+    if(!details){
+        res.status(404).json({error : "No such instructor"})
+    } else{
+        res.json(details);
+    }
+}
+
+//delete instructor 
+const deleteInstructor = async (req,res) =>{
+    const {id} = req.params
+
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        res.status(404).json({error: 'No such instructor'})
+    }
+
+    const instructor = await instructors.findByIdAndDelete(id)
+
+    if(!instructors){
+        res.status(404).json({error : "No such instructor"})
+    } else{
+        res.json(instructors);
+    }
+}
+
+
+//update instructor details
+const updateInstructor= async (req,res)=>{
+    const {id} = req.params
+
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        res.status(404).json({error: 'No such instructor'})
+    }
+
+    const instructor = await instructors.findOneAndUpdate({_id: id},{
+        ...req.body
+    })
+
+    if(!instructor){
+        res.status(404).json({error : "No such instructor"})
+    } else{
+        res.json(instructor);
+    }
+}
+
 module.exports = {
     createInstructor,
+    getAllInstructorDetails,
+    getSingleInstructorDetails,
+    updateInstructor,
+    deleteInstructor
 
 }
