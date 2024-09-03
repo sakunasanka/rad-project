8ab3c182642436080290ca3feddc54818520f7d1
shehara Gamage <140165643+sheharagamage@users.noreply.github.com>
2024-09-03 11:54:08 +0530

    create login.js

--- frontend/src/hooks/useLogin.js
+++ frontend/src/hooks/useLogin.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { useAuthContext } from './useAuthContext';
 
+
 export const useLogin = () => {
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(null);
