db3964718469ccaf853c44dd7f484851ef4ee52d
sakunasanka <sakunasanka@gmail.com>
2024-09-03 10:56:18 +0530

    Create routes in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { useAuthContext } from './hooks/useAuthContext';
 
-const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+import Instructors from './pages/Instructors';
 
-export default App
+function App() {
+    const { user } = useAuthContext();
+  
+    return (
+      <div className="App">
+        <BrowserRouter>
+
+          <div className="pages">
+            <Routes>
+              
+              <Route
+                path="/instructors"
+                element={user ? <Instructors /> : <Navigate to="/login" />}
+              />
+              <Route
+                path="/"
+                element={user ? <Home /> : <Navigate to="/login" />}
+              />
+              <Route
+                path="/login"
+                element={!user ? <Login /> : <Navigate to="/" />}
+              />
+              <Route
+                path="/signup"
+                element={!user ? <Signup /> : <Navigate to="/" />}
+              />
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
+  
+  export default App;
+  
