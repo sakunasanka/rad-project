5f27c8d0d3b96826c1fe239788c798c645bd6f07
sakunasanka <sakunasanka@gmail.com>
2024-09-04 23:11:33 +0530

    Create userController

--- backend/controllers/teacherController.js
+++ backend/controllers/teacherController.js
@@ -1,89 +1,81 @@
-const teachers = require('../module/teacherStructure');
-const mongoose = require('mongoose');
+const teachers = require('../module/teacherStructure')
+const mongoose = require('mongoose')
 
 //create teacher
-const createTeacher = async (req, res) => {
-  const { name, country, email, birth_Date, Subjects } = req.body;
-  try {
-    const teacher = await teachers.create({
-      name,
-      country,
-      email,
-      birth_Date,
-      Subjects,
-    });
-    res.json(teacher);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-};
+const createTeacher = async (req,res)=>{
+    const {name,country,email,birth_Date,Subjects} = req.body
+    try{
+        const teacher = await teachers.create({name,country,email,birth_Date,Subjects})
+        res.json(teacher);
+    } catch(error){
+        res.status(400).json({error: error.message})
+    } 
+}
 
 //get all teachers details
-const getAllteacherDetails = async (req, res) => {
-  const details = await teachers.find({}).sort({ createdAt: -1 });
-
-  res.json(details);
-};
+const getAllteacherDetails = async (req,res) =>{
+    const details = await teachers.find({}).sort({createdAt : -1})
+ 
+    res.json(details);
+}
 
 //get single student details
-const getSingleTeacherDetails = async (req, res) => {
-  const { id } = req.params;
+const getSingleTeacherDetails = async (req,res) =>{
+    const {id} = req.params
 
-  if (!mongoose.Types.ObjectId.isValid(id)) {
-    res.status(404).json({ error: 'No such teacher' });
-  }
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        res.status(404).json({error: 'No such teacher'})
+    }
 
-  const details = await teachers.findById(id);
-  if (!details) {
-    res.status(404).json({ error: 'No such teacher' });
-  } else {
-    res.json(details);
-  }
-};
+    const details = await teachers.findById(id)
+    if(!details){
+        res.status(404).json({error : "No such teacher"})
+    } else{
+        res.json(details);
+    }
+}
 
-//delete student
-const deleteTeacher = async (req, res) => {
-  const { id } = req.params;
+//delete student 
+const deleteTeacher = async (req,res) =>{
+    const {id} = req.params
 
-  if (!mongoose.Types.ObjectId.isValid(id)) {
-    res.status(404).json({ error: 'No such teacher' });
-  }
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        res.status(404).json({error: 'No such teacher'})
+    }
 
-  const teacher = await teachers.findByIdAndDelete(id);
+    const teacher = await teachers.findByIdAndDelete(id)
 
-  if (!teacher) {
-    res.status(404).json({ error: 'No such teacher' });
-  } else {
-    res.json(teacher);
-  }
-};
+    if(!teacher){
+        res.status(404).json({error : "No such teacher"})
+    } else{
+        res.json(teacher);
+    }
+}
+
 
 //update student details
-const updateTeacher = async (req, res) => {
-  const { id } = req.params;
+const updateTeacher = async (req,res)=>{
+    const {id} = req.params
 
-  if (!mongoose.Types.ObjectId.isValid(id)) {
-    res.status(404).json({ error: 'No such teacher' });
-  }
-
-  const teacher = await teachers.findOneAndUpdate(
-    { _id: id },
-    {
-      ...req.body,
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        res.status(404).json({error: 'No such teacher'})
     }
-  );
 
-  if (!teacher) {
-    res.status(404).json({ error: 'No such teacher' });
-  } else {
-    res.json(teacher);
-  }
-};
+    const teacher = await teachers.findOneAndUpdate({_id: id},{
+        ...req.body
+    })
+
+    if(!teacher){
+        res.status(404).json({error : "No such teacher"})
+    } else{
+        res.json(teacher);
+    }
+}
 
 module.exports = {
-  createTeacher,
-  getAllteacherDetails,
-  getSingleTeacherDetails,
-  updateTeacher,
-  deleteTeacher,
-};
+    createTeacher,
+    getAllteacherDetails,
+    getSingleTeacherDetails,
+    updateTeacher,
+    deleteTeacher
+}
