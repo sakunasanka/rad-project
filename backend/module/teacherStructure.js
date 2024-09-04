b5c2836453356499283053b529560518407090ea
Chamudi Upeka <129956853+chamudiupeka@users.noreply.github.com>
2024-09-04 12:19:20 +0530

    Update  the Teacher module

---
+++ backend/module/teacherStructure.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator');
+
+const Schema = mongoose.Schema
+
+const emailValidator = {
+    validator: function (value) {
+        // Use a regular expression to validate the email format
+        const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+        return emailRegex.test(value);
+    },
+    message: 'Invalid email format'
+};
+
+
+const teacherSchema = new Schema({
+    name : {
+        type : String,
+        required : true,
+        unique : true,
+    },
+    country :{
+        type : String,
+        required : true
+    },
+    birth_Date :{
+        type : Date,
+        required : true
+    },
+    email :{
+        type : String,
+        required : true,
+        unique:true,
+        validate: emailValidator
+    },
+    Subjects :{
+        type : String,
+        required : true
+    }
+},{Timestamp:true})
+
+teacherSchema.plugin(uniqueValidator);
+
+module.exports = mongoose.model('teacher',teacherSchema); 
